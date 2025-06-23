5ceae78e8c2d1e55189606659ba05c27a5d5e4a9
sedfarouk <sadikalhanssah@gmail.com>
2025-06-23 22:41:14 +0000

    fix: change base in config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: './',
+  base: '/audiophile-ecommerce/',
   plugins: [react()],
   resolve: {
     alias: {
